b1ce621012be86a208b6e7b7c168392bb80ef204
tomi <tomisin18@gmail.com>
2020-08-26 23:30:09 +0100

    Add auth view controllers

--- SpendR/Auth/VieControllers/LoginViewController.swift
+++ SpendR/Auth/VieControllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  SpendR
 //
 //  Created by OYEGOKE TOMISIN on 26/08/2020.
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 }
