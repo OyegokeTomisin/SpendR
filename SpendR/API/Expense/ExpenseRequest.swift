3b09b907b2531a74a98560f13fc0552a1d9a6b4a
tomi <tomisin18@gmail.com>
2020-09-01 06:45:58 +0100

    Add expense requests

--- SpendR/API/Expense/ExpenseRequest.swift
+++ SpendR/API/Expense/ExpenseRequest.swift
@@ -7,20 +7,3 @@
 //
 
 import Foundation
-
-struct ExpenseRequest {
-
-    private let router = Router<ExpenseAPI>()
-
-    func createEexpense(expense: Expense, completion: @escaping NetworkRouterCompletion) {
-        router.request(.createExpense(expense: expense), completion: completion)
-    }
-
-    func fetchExpense(completion: @escaping NetworkRouterCompletion) {
-        router.request(.fetchExpenses, completion: completion)
-    }
-
-    func deleteExpense(expenseId: String, completion: @escaping NetworkRouterCompletion) {
-        router.request(.deleteExpense(expenseId: expenseId), completion: completion)
-    }
-}
